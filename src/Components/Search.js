8d6413a9f6699af86309aabeaedfa502e22d77f5
Simran Mistry <76590641+mistrysimran@users.noreply.github.com>
2023-09-02 22:19:26 +0530

    Update Search.js

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -13,7 +13,6 @@ const Search = ({setSearchQuery}) => {
         <Grid columns={1} textAlign="center" className="search-box">
             <Grid.Column>
                 <h2 className="search-heading">Search Recipes with <span style={{ color: '#2185d0' }}>Recipe Radar</span></h2>
-                {/* <h4>Input items separated by comma</h4> */}
                 <Form onSubmit={onFormSubmit}>
                     <Input
                         className="inp"
